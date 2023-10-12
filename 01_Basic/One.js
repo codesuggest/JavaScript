b1182d5e48efb66e74ee6c8c36a9f4f13f8c7b61
Ajay Kumar Sagar <jkmrsgr@gmail.com>
2023-10-12 23:10:53 +0530

    JavaScript Variables

---
+++ 01_Basic/One.js
@@ -0,0 +1,6 @@
+const RollNumber=14000500;
+let StudentName='Ajay Kumar';
+var YoutubeChannel='Code Suggest';
+//RollNumber=100;
+YoutubeChannel="Please Subscribe Code Suggest Youtube Channel";
+console.table([RollNumber,StudentName,YoutubeChannel]);
